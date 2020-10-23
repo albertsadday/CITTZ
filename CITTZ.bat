4d1a673f1b32c68616c9e2de9969057f14eecef1
Pedro Vega <67062049+wrrulos@users.noreply.github.com>
2020-10-23 11:35:56 -0300

    Update CITTZ.bat

--- CITTZ.bat
+++ CITTZ.bat
@@ -1,6 +1,6 @@
 del cittz-temp.bat
 echo @echo off >> cittz-temp.bat
-echo echo set ubicacion= %cd% >> cittz-temp.bat
+echo set ubicacion= %cd% >> cittz-temp.bat
 echo. >> cittz-temp.bat
 type cod1.txt >> cittz-temp.bat
 type cod2.txt >> cittz-temp.bat
