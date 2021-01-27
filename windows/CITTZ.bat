abbc9e26c485447bae3595a85559898ab3371547
Pedro Vega <67062049+wrrulos@users.noreply.github.com>
2021-01-27 17:41:50 -0300

    Delete windows directory

--- windows/CITTZ.bat
+++
@@ -1,7 +0,0 @@
-del cittz-temp.bat
-echo @echo off >> cittz-temp.bat
-echo set ubicacion= %cd% >> cittz-temp.bat
-echo. >> cittz-temp.bat
-type cod1.txt >> cittz-temp.bat
-type cod2.txt >> cittz-temp.bat
-start cittz-temp.bat
